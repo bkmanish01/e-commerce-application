d82ffe42396943c5235e087619b8cef650d877c9
bkmanish01 <hmcmanish01@gmail.com>
2022-08-30 15:18:22 -0600

    changed BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './styles/index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </Router>
 );
 
