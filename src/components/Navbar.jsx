85e443ff1a3e3ae9c8def499ddf439340e3e4731
bkmanish01 <hmcmanish01@gmail.com>
2022-09-01 16:17:27 -0600

    fixed collapse navbar issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
     return (
         <div className="bg-dark" id="layout">
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3 shadow-sm">
+            <nav className="navbar navbar-expand-lg  navbar-dark bg-dark py-3 shadow-sm">
                 <div className="container">
                     <Link className="navbar-brand fw-bold" to="/">Global Mart</Link>
                     <button 
@@ -29,31 +29,31 @@ const Navbar = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-                        <li className="nav-item" key="1">
-                            <Link className="nav-link active" aria-current="page" to="home">Home</Link>
-                        </li>
-                        <li className="nav-item" key="2">
-                            <Link className="nav-link"  to="about">About</Link>
-                        </li>
-                        <li className="nav-item" key="3">
-                            <Link className="nav-link"  to="products">Products</Link>
-                        </li>
-                        <li className="nav-item" key="4">
-                            <Link className="nav-link"  to="contact">Contact</Link>
-                        </li>
-                    </ul>
-                    <form className="button">
-                        <NavLink to="/login" className="btn">
-                            <i className="fa fa-sign-in me-1 text-white"> LogIn</i>
-                        </NavLink>
-                        <NavLink to="/signup" className="btn ms-2">
-                            <i className="fa fa-user-plus me-1 text-white"> SignUp</i>
-                        </NavLink>
-                        <NavLink to="/cart" className="btn ms-2">
-                            <i className="fa fa-shopping-cart me-1 text-white"> cart ({state.length})</i>
-                        </NavLink>
-                    </form>
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item" key="1">
+                                <Link className="nav-link active" aria-current="page" to="home">Home</Link>
+                            </li>
+                            <li className="nav-item" key="2">
+                                <Link className="nav-link"  to="about">About</Link>
+                            </li>
+                            <li className="nav-item" key="3">
+                                <Link className="nav-link"  to="products">Products</Link>
+                            </li>
+                            <li className="nav-item" key="4">
+                                <Link className="nav-link"  to="contact">Contact</Link>
+                            </li>
+                        </ul>
+                        <form className="button">
+                            <NavLink to="/login" className="btn">
+                                <i className="fa fa-sign-in me-1 text-white"> LogIn</i>
+                            </NavLink>
+                            <NavLink to="/signup" className="btn ms-2">
+                                <i className="fa fa-user-plus me-1 text-white"> SignUp</i>
+                            </NavLink>
+                            <NavLink to="/cart" className="btn ms-2">
+                                <i className="fa fa-shopping-cart me-1 text-white"> cart ({state.length})</i>
+                            </NavLink>
+                        </form>
                     </div>
                 </div>
             </nav>
@@ -67,3 +67,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
